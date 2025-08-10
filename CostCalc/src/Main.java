b9ba8024c5748d2728926dcde0de55d0a5ee70e3
HurleySk <sk.hurley@gmail.com>
2025-08-10 14:15:05 -0400

    starting md calc project

---
+++ CostCalc/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    private Boolean shouldExit = False;
+
+    public static void main(String[] args) {
+        int input = new Scanner(System.in)
+
+        while (!shouldExit) {
+
+        }
+    }
+}
